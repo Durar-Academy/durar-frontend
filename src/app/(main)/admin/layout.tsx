e097885c647423f6831f3f251d1713b36d980643
Muiz Haruna <muizharuna55@gmail.com>
2025-02-02 00:09:25 +0100

    feat: added sidebar

--- src/app/(main)/admin/layout.tsx
+++ src/app/(main)/admin/layout.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
-
-import { SidebarLinks } from "@/components/admin/sidebar-links";
+import { AdminSidebar } from "@/components/admin/sidebar";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="w-full h-screen overflow-hidden flex bg-offwhite">
-      <aside className="w-[260px] shrink-0 h-full bg-green p-8">
-        <div className="relative w-32 h-10">
-          <Image src={"/logo-white.svg"} fill alt="Durar Logo" className="object-cover object-center" />
-        </div>
-
-        <div className="mt-[60px]">
-          <SidebarLinks />
-        </div>
-      </aside>
+      <AdminSidebar />
 
       <main className="p-6">{children}</main>
     </section>
