4f47a4a4075e5c6894980d920600239337fd0f7b
Muiz Haruna <muizharuna55@gmail.com>
2025-03-18 11:57:07 +0100

    wip: added students route and layout

---
+++ src/components/student/sidebar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import { SidebarLinks } from "@/components/student/sidebar-links";
+
+export function StudentSidebar() {
+  return (
+    <aside className="w-[260px] shrink-0 h-full bg-green p-8">
+      <div className="relative w-32 h-10">
+        <Image
+          src={"/logo-white.svg"}
+          fill
+          alt="Durar Logo"
+          className="object-cover object-center"
+        />
+      </div>
+
+      <div className="mt-[60px]">
+        <SidebarLinks />
+      </div>
+    </aside>
+  );
+}
